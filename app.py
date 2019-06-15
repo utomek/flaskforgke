43d3d0d3ec9f8874957406f2387a407b048377a9
Tomek U <utomek@gmail.com>
2019-06-15 11:18:04 +0200

    added app.py and dockerfile

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<h1>Greetings </h1>"
+    return html
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
+    
+
---
+++ dockerfile
@@ -0,0 +1,9 @@
+FROM python:2.7-slim
+WORKDIR /app
+ADD . /app
+
+RUN pip install -r requirements.txt
+
+EXPOSE 80
+
+CMD ("python", "app.py")
